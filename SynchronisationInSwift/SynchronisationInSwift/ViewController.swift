67ec20616fa1793e35d9e94f7735cce1838d80c4
“monica-futuristic” <82701296+monica-futuristic@users.noreply.github.com>
2022-04-20 11:43:31 +0530

    Basic project setup

---
+++ SynchronisationInSwift/SynchronisationInSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SynchronisationInSwift
+//
+//  Created by Laxmi Ganesh on 20/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
