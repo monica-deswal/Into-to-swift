d539378e8c9fe6cd98a8f64df4af8ee932c3e150
“monica-futuristic” <82701296+monica-futuristic@users.noreply.github.com>
2022-04-28 10:36:05 +0530

    basic project setup

---
+++ StructAndClasses/StructDiffClass/StructDiffClass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StructDiffClass
+//
+//  Created by Laxmi Ganesh on 28/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
